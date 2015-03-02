44f2c73a2024b4c0474e62eae33302d2debce157
Katrina Verey <kn.verey@gmail.com>
2015-03-01 20:31:43 -0500

    Read from file, write to socket

--- chat-start.c
+++ chat-start.c
@@ -4,37 +4,49 @@
 #include <errno.h>
 #include <string.h>
 #include <sys/stat.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netdb.h>
 
 #define READ_CHUNKS 100
 
 int main(void){
+  int sockfd;
+  struct addrinfo hints, *res;
   int readme;
-  int writeme;
   int actually_read;
   char buffer[READ_CHUNKS];
+
+  memset(&hints, 0, sizeof hints);
+  hints.ai_family = AF_UNSPEC;
+  hints.ai_socktype = SOCK_STREAM;
+  getaddrinfo("0.0.0.0", "1234", &hints, &res);
 
   readme = open("readme.txt", O_RDONLY);
 
   if (readme >= 0) {
-    writeme = open("test", O_WRONLY | O_CREAT | O_TRUNC, S_IRUSR | S_IWUSR | S_IRGRP | S_IROTH);
+    sockfd = socket(res->ai_family, res->ai_socktype, res->ai_protocol);
 
-    if (writeme > 0) {
+    if (sockfd > 0) {
+      connect(sockfd, res->ai_addr, res->ai_addrlen);
+
       while(1) {
         actually_read = read(readme, buffer, READ_CHUNKS);
         if (actually_read==0)
           break;
-        write(writeme, buffer, actually_read);
+        send(sockfd, buffer, actually_read, 0);
       }
 
-      close(writeme);
+      close(sockfd);
 
     } else {
-      printf("Write file open error %s\n", strerror(errno));
+      printf("Socket creation error %s\n", strerror(errno));
     }
 
     close(readme);
-
   } else {
     printf("Read file open error %s\n", strerror(errno));
   }
+
+  freeaddrinfo(res);
 }
